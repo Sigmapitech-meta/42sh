5abc698e66b07109f525710f58be4b8ea3953b38
Sigmanificient <edhyjox@gmail.com>
2023-04-29 04:41:44 +0200

    :sparkles: Add argument handling

--- src/commands/location_builtins.c
+++ src/commands/location_builtins.c
@@ -15,35 +15,57 @@
 #include "shell/utils.h"
 #include "utils/autofree.h"
 
-void builtin_which(context_t *ctx)
+static
+bool_t is_builtin(char *cmd_name)
 {
-    bool_t found = FALSE;
-    command_t *cmd = ctx->cmd;
-    char *cmd_name = cmd->argv[1];
-    char *path = getenv("PATH");
-    AUTOFREE char *cmd_path = NULL;
+    char *builtin_name;
 
-    for (int i = 0; i < BUILTIN_COUNT && !found; i++)
-        found = !strncmp(cmd_name, BUILTINS[i].name, strlen(BUILTINS[i].name));
-    if (found) {
-        printf("%s: shell built-in command.\n", cmd_name);
-        return;
+    for (int i = 0; i < BUILTIN_COUNT; i++) {
+        builtin_name = BUILTINS[i].name;
+        if (!strncmp(cmd_name, builtin_name, strlen(builtin_name)))
+            return TRUE;
     }
-    if (!path)
-        return;
-    cmd_path = path_find_access(path, cmd_name);
+    return FALSE;
+}
+
+static void check_access(char *path, char *cmd_name)
+{
+    AUTOFREE char *cmd_path = path_concat(path, cmd_name);
+
     if (!cmd_path)
-        eprintf("%s: Command not found.\n", cmd_name);
-    else
+        return;
+    if (!access(cmd_path, F_OK))
         printf("%s\n", cmd_path);
 }
 
+void builtin_which(context_t *ctx)
+{
+    command_t *cmd = ctx->cmd;
+    AUTOFREE char *cmd_path = NULL;
+    char *path = getenv("PATH");
+
+    if (cmd->argc < 2) {
+        eprintf("which: Too few arguments.");
+        return;
+    }
+    for (int i = 1; i < cmd->argc; i++) {
+        if (is_builtin(cmd->argv[i]))
+            printf("%s: shell built-in command.\n", cmd->argv[i]);
+        if (!path)
+            return;
+        cmd_path = path_find_access(path, cmd->argv[i]);
+        if (!cmd_path)
+            eprintf("%s: Command not found.\n", cmd->argv[i]);
+        else
+            printf("%s\n", cmd_path);
+    }
+}
+
 static
-void builtin_where_search(context_t *ctx, char *cmd_name)
+void builtin_where_search(char *cmd_name)
 {
     AUTOFREE char *path_copy = NULL;
     char *path = getenv("PATH");
-    char *cmd_path = NULL;
     char *search_path;
     char *checkpoint;
 
@@ -54,22 +76,24 @@ void builtin_where_search(context_t *ctx, char *cmd_name)
         return;
     search_path = strtok_r(path_copy + 5, ":", &checkpoint);
     while (search_path) {
-        if (cmd_path)
-            free(cmd_path);
-        cmd_path = path_concat(search_path, cmd_name);
-        if (cmd_name && !access(cmd_path, F_OK))
-            printf("%s\n", cmd_path);
+        check_access(search_path, cmd_name);
         search_path = strtok_r(NULL, ":", &checkpoint);
     }
 }
 
 void builtin_where(context_t *ctx)
 {
+    char *cmd_name;
     command_t *cmd = ctx->cmd;
-    char *cmd_name = cmd->argv[1];
 
-    for (int i = 0; i < BUILTIN_COUNT; i++)
-        if (!strncmp(cmd_name, BUILTINS[i].name, strlen(BUILTINS[i].name)))
+    if (cmd->argc < 2) {
+        eprintf("where: Too few arguments.");
+        return;
+    }
+    for (int i = 0; i < cmd->argc; i++) {
+        cmd_name = cmd->argv[i];
+        if (is_builtin(cmd_name))
             printf("%s is a shell built-in\n", cmd_name);
-    builtin_where_search(ctx, cmd_name);
+        builtin_where_search(cmd_name);
+    }
 }
