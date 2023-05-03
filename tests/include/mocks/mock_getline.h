787f6fd5cde728d06da06b9a452f13927eed6b8b
Sigmanificient <edhyjox@gmail.com>
2023-05-03 02:35:29 +0200

    :white_check_mark: Improve get_line test

--- tests/include/mocks/mock_getline.h
+++ tests/include/mocks/mock_getline.h
@@ -9,34 +9,31 @@
     #define MOCK_GETLINE_H_
 
     #include "epitech/norm.h"
-#include "utils/attributes.h"
+    #include "utils/attributes.h"
 
-#define real_getline __real_getline
+    #define real_getline __real_getline
     #define wrap_getline __wrap_getline
 
-    #define GETLINE_TOGGLE (*getline_activation_ptr())
-    #define ARR_SIZE(arr) (sizeof (arr) / sizeof(arr[0]))
+    #define GETLINE_DATA (getline_mock_ptr())
 
     #include <stddef.h>
 
 typedef FILE file_t;
 
+typedef enum {
+    BROKEN,
+    NORMAL,
+    TABLE
+} gl_mode_t;
+
+typedef struct {
+    gl_mode_t mode;
+    char **table;
+} gl_mock_t;
+
+gl_mock_t *getline_mock_ptr(void);
+
 ssize_t wrap_getline(char **line, size_t *read, file_t *stream);
 ssize_t real_getline(char **line, size_t *read, file_t *stream);
-
-void use_mock_getline(void);
-void use_normal_getline(void);
-
-USED
-static const char *GETlINE_TABLE[] = {
-    "Hello\n",
-    "this\n",
-    "is\n",
-    "my\n",
-    "friend\n",
-    "Albert!\n"
-};
-
-static const int GETLINE_TABLE_LENGTH = ARR_SIZE(GETlINE_TABLE);
 
 #endif /* !MOCK_GETLINE_H_ */
--- tests/mocks/mock_getline.c
+++ tests/mocks/mock_getline.c
@@ -9,32 +9,34 @@
 #include <bits/types/FILE.h>
 #include <string.h>
 
-#include "epitech.h"
 #include "mocks/mock_getline.h"
+#include "utils/sentinel.h"
 
-bool_t *getline_activation_ptr(void)
+gl_mock_t *getline_mock_ptr(void)
 {
-    static bool_t enabled = FALSE;
+    static gl_mock_t data = { .mode = NORMAL, 0};
 
-    return &enabled;
+    return &data;
 }
 
 ssize_t wrap_getline(char **line, size_t *read, file_t *stream)
 {
     static int index = 0;
+    char *read_line;
 
-    if (!GETLINE_TOGGLE)
-        return real_getline(line, read, stream);
-    *line = strdup(GETlINE_TABLE[index++ % GETLINE_TABLE_LENGTH]);
-    return (ssize_t)strlen(*line);
-}
-
-void use_mock_getline(void)
-{
-    GETLINE_TOGGLE = TRUE;
-}
-
-void use_normal_getline(void)
-{
-    GETLINE_TOGGLE = FALSE;
+    if (GETLINE_DATA->mode == BROKEN) {
+        *line = NULL;
+        return W_SENTINEL;
+    }
+    if (GETLINE_DATA->mode == TABLE) {
+        read_line = GETLINE_DATA->table[index++];
+        if (!read_line) {
+            *line = read_line;
+            return (ssize_t) strlen(*line);
+        } else {
+            *line = NULL;
+            return W_SENTINEL;
+        }
+    }
+    return real_getline(line, read, stream);
 }
