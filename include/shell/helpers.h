ee868a19b8a2b0ea35aae9e232ea044b1ddb6c23
Ciznia <gabriel.hosquet@epitech.eu>
2023-05-05 17:55:58 +0200

    :recycle: fix minor issues in header file

--- include/shell/helpers.h
+++ include/shell/helpers.h
@@ -9,7 +9,7 @@
     #define HELPERS_H_
 
 long file_get_size(char const *filepath);
-char *file_read_fd(int fd, long filesize);
+char *file_read_fd(int fd, size_t filesize);
 char *file_read(char *filepath);
 
 #endif /* !HELPERS_H_ */
