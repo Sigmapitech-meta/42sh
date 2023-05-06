d28b08627e2a99fd78696446b8b45cb6bd7100b2
Fenriir42 <raid3r2205@gmail.com>
2023-05-06 17:42:33 +0200

    :sparkles: add test simple env

---
+++ tests/e2e/env/test_env.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** test_command_ls.c
+*/
+
+#include "epitech/sputnik.h"
+#include "run_shell_command.h"
+
+TEST_STD(run_shell_command, env)
+$ {
+    CR_ASSERT_EQ(run_shell_command("env"), 0);
+}
