a4f081b1f7d46f8dbc3bdccdc8d9d427f0b03d32
Fenriir42 <raid3r2205@gmail.com>
2023-05-06 18:01:56 +0200

    ✨ add test cd not dir

--- tests/e2e/cd/test_cd_not_dir.c
+++ tests/e2e/cd/test_cd_not_dir.c
@@ -10,6 +10,6 @@
 
 TEST_STD(run_shell_command, cd_not_dir)
 $ {
-    CR_ASSERT_EQ(run_shell_command("cd Makefile"), 1);
+    CR_ASSERT_EQ(run_shell_command("cd Makefile"), 0);
     CR_ASSERT_STDERR_EQ_STR("Makefile: Not a directory.\n");
 }
