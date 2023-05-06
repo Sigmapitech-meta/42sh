d7ea54d01fe25bd71ffd37ef60592bd07e4d4eb5
Fenriir42 <raid3r2205@gmail.com>
2023-05-06 18:02:12 +0200

    ✨ add test setenv

---
+++ tests/e2e/env/test_setenv.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** test_command_ls.c
+*/
+
+#include "epitech/sputnik.h"
+#include "run_shell_command.h"
+
+TEST_STD(run_shell_command, setenv_Yousk2)
+$ {
+    CR_ASSERT_EQ(run_shell_command("setenv Yousk2 couscous"), 0);
+    CR_ASSERT_STDERR_EQ_STR("");
+}
