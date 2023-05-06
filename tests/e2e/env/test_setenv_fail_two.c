9f2301f505ef6623c3ffa831efbc2835dd42b85d
Fenriir42 <raid3r2205@gmail.com>
2023-05-06 18:02:26 +0200

    ✨ add second setenv error test

---
+++ tests/e2e/env/test_setenv_fail_two.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** 42sh
+** File description:
+** test_command_ls.c
+*/
+
+#include "epitech/sputnik.h"
+#include "run_shell_command.h"
+
+TEST_STD(run_shell_command, setenv_fail_two)
+$ {
+    CR_ASSERT_EQ(run_shell_command("setenv Y°sk2"), 0);
+    CR_ASSERT_STDERR_EQ_STR("setenv: Variable name must contain alphanumeric characters.\n");
+}
