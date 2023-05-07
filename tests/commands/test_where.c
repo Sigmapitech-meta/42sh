3c68d403b8b53d70538fa736a5fdfc10a3c961ce
Sigmanificient <edhyjox@gmail.com>
2023-05-07 19:35:58 +0200

    :recycle: Rewrite where & which tests

--- tests/commands/test_where.c
+++ tests/commands/test_where.c
@@ -5,11 +5,36 @@
 ** test_where_fail.c
 */
 
+#include <unistd.h>
 #include "sputnik.h"
 #include "run_shell_command.h"
+#include "utils/sentinel.h"
+#include "epitech.h"
 
-TEST_STD(run_command_where, where)
+TEST_STD(run_command_where, no_arguments)
 $ {
-    CR_ASSERT_EQ(run_shell_command("where"), 0);
+    CR_ASSERT_EQ(run_shell_command("where"), 1);
     CR_ASSERT_STDERR_EQ_STR("where: Too few arguments.\n");
+}
+
+TEST_STD(run_command_where, ls)
+$ {
+    char *buff;
+    FILE *fake_stdout = cr_get_redirected_stdout();
+    size_t track = 0;
+    int count = 0;
+    int read;
+
+    CR_ASSERT_EQ(run_shell_command("which ls"), 0);
+    do {
+        read = getline(&buff, &track, fake_stdout);
+        if (IS_SENTINEL_OF(read, ssize_t))
+            break;
+        CR_ASSERT(
+            strncmp(buff + read - 4, "/ls\n", 2),
+            "Should end with `.../ls\n`."
+        );
+    } while (++count);
+    if (!count)
+        cr_skip("Cannot read fake stdout");
 }
--- tests/commands/test_which.c
+++ tests/commands/test_which.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include "sputnik.h"
 #include "run_shell_command.h"
+#include "utils/sentinel.h"
 
 TEST_STD(run_command_which, no_argument)
 $ {
@@ -17,6 +18,34 @@ $ {
 
 TEST_STD(run_command_which, simple_ls)
 $ {
+    char buff[1024] = { '\0' };
+    FILE *fake_stdout = cr_get_redirected_stdout();
+    int state;
+
     CR_ASSERT_EQ(run_shell_command("which ls"), 0);
-    CR_ASSERT_STDOUT_EQ_STR("/usr/bin/ls\n");
+    state = read(fake_stdout->_fileno, buff, 1023);
+    if (IS_SENTINEL_OF(state, ssize_t))
+        cr_skip("Cannot read fake stdout");
+    buff[state] = '\0';
+    CR_ASSERT(
+        strncmp(buff + state - 4, "/ls\n", 2),
+        "Should end with `.../ls\n`."
+    );
+}
+
+TEST_STD(run_command_which, builtin_command)
+$ {
+    char buff[1024] = { '\0' };
+    FILE *fake_stdout = cr_get_redirected_stdout();
+    int state;
+
+    CR_ASSERT_EQ(run_shell_command("which which"), 0);
+    state = read(fake_stdout->_fileno, buff, 1023);
+    if (IS_SENTINEL_OF(state, ssize_t))
+        cr_skip("Cannot read fake stdout");
+    buff[state] = '\0';
+    CR_ASSERT(
+        strncmp(buff + state - 4, "/ls\n", 2),
+        "which: shell built-in command."
+    );
 }
