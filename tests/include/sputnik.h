9373b6d7f16a435a8ab0280ce19c207beee93ef8
Sigmanificient <edhyjox@gmail.com>
2023-05-08 06:02:40 +0200

    :white_check_mark: Add new tests scenarios

--- tests/include/sputnik.h
+++ tests/include/sputnik.h
@@ -30,6 +30,8 @@
     #define CR_ASSERT_STDOUT_EQ_STR(...) cr_assert_stdout_eq_str(__VA_ARGS__)
     #define CR_ASSERT_STDERR_EQ_STR(...) cr_assert_stderr_eq_str(__VA_ARGS__)
 
+    #define CR_ASSERT_STDOUT_NEQ_STR(...) cr_assert_stdout_neq_str(__VA_ARGS__)
+
     #define TEST_SETUP(before) .init EQ before
     #define TEST_TEARDOWN(after) .fini EQ after
 
