b826b14c6367eb99f97bf4a289167d4c3be120fc
Fenriir42 <raid3r2205@gmail.com>
2023-05-09 20:15:02 +0200

    :wrench: add Doxyfile config

--- include/list.h
+++ include/list.h
@@ -34,16 +34,66 @@ struct list_s {
     ssize_t size;
 };
 
+/**
+ * @brief append a value to the list
+ *
+ * @param list the list
+ * @param value the value
+ * @return ssize_t : the size of the list
+ */
 ssize_t list_append(list_t *list, void *value);
+/**
+ * @brief append a node to the list
+ *
+ * @param list the list
+ * @param node the node
+ * @return ssize_t : the size of the list
+ */
 ssize_t list_append_node(list_t *list, list_node_t *node);
 
+/**
+ * @brief get a value from the list
+ *
+ * @param list the list
+ * @param index the index
+ * @return void* : the value
+ */
 void *list_get(list_t *list, uint_t index);
+/**
+ * @brief get a node from the list
+ *
+ * @param list the list
+ * @param index the index
+ * @return list_node_t* : the node
+ */
 list_node_t *list_get_node(list_t *list, uint_t index);
 
+/**
+ * @brief remove a value from the list
+ *
+ * @param list the list
+ * @param index the index
+ */
 void list_remove(list_t *list, uint_t index);
+/**
+ * @brief remove a node from the list
+ *
+ * @param list the list
+ * @param node the node
+ */
 void list_remove_node(list_t *list, list_node_t *node);
+/**
+ * @brief clear the list
+ *
+ * @param list the list
+ */
 void list_clear(list_t *list);
 
+/**
+ * @brief create a list
+ *
+ * @return USED* : the list
+ */
 static inline
 USED list_t *list_create(void)
 {
