c5942a361351703a8fd0b34411fdb5ef8a22d892
Fenriir42 <raid3r2205@gmail.com>
2023-05-09 21:37:31 +0200

    :bulb: add documentation
    Co-authored-by: Yohann Boniface <edhyjox@gmail.com>

--- include/shell/shell.h
+++ include/shell/shell.h
@@ -14,6 +14,10 @@ extern char **environ;
 
 static const size_t MINIMAL_INPUT_CHECK = 2;
 
+/**
+ * @brief Prints the usage of the shell
+ *
+ */
 static const char USAGE[] = (
     "Minishell 2 - Usage: ./mysh [-h]\n"
     "\nAll commands in the path are available.\n"
@@ -44,18 +48,27 @@ typedef struct context_s {
     unsigned short status;
 } context_t;
 
+/** @brief Run a command in the shell */
 int command_run_subprocess(context_t *ctx);
 
+/** @brief main loop */
 int shell_run_from_env(char **env);
+/** @brief run a command in the shell* */
 void shell_run_from_ctx(context_t *ctx);
 
+/** @brief read user input */
 bool_t shell_read_line(context_t *ctx);
+/** @brief run a command in the shell */
 void shell_evaluate(context_t *ctx);
 
+/** @brief display prompt */
 void prompt_display(void);
 
+/** @brief Retrieve string composed by `$key = $val` */
 char *env_get_setter(char *key, char *value);
+/** @brief free a key */
 void env_free_key(char *key, char **original_env);
+/** @brief free the env */
 void env_free(char **original_env);
 
 #endif /* !SHELL_H */
--- include/shell/utils.h
+++ include/shell/utils.h
@@ -16,15 +16,22 @@ enum {
     SEGFAULT_CORE_DUMP = SEGFAULT | _CORE_DUMP
 };
 
+/** @brief Concatenate two paths */
 char *path_concat(char *left, char *right);
+/** @brief Retrieve the path of a command */
 char *path_find_cmd(char *cmd);
+/** @brief Retrieve the command path if it can be accessed */
 char *path_find_access(char *path, char *cmd);
 
+/** @brief Count the number of parameters */
 int param_count(const char *line);
+/** @brief Fill the list of parameters for a command string */
 void param_fill(char **params, char *string);
 
+/** @brief Print the status message for a given code */
 void status_show(int status);
 
+/** @brief set a environment variable and handle necessary cleanups */
 void putenv_safe(char *key, char *value, char **original_env);
 
 #endif /* !UTILS_H_ */
