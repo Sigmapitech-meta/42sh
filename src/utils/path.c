cea712e7001e5ac99c61c13ead132ce5d9b26c0e
Sigmanificient <edhyjox@gmail.com>
2023-05-10 10:43:00 +0200

    :bug: Fix path error

--- src/utils/path.c
+++ src/utils/path.c
@@ -12,6 +12,7 @@
 
 #include "utils/debug_mode.h"
 #include "utils/cleanup.h"
+#include "base.h"
 
 char *path_concat(char *left, char *right)
 {
@@ -30,21 +31,17 @@ char *path_concat(char *left, char *right)
 
 char *path_find_access(char *path, char *cmd)
 {
-    AUTOFREE char *path_copy = strdup(path);
-    char *search_path;
-    char *checkpoint;
-    char *cmd_path;
+    char *rest;
+    AUTOFREE char *p_copy = strdup(path);
+    char *p_search = NULL_OR(p_copy, strtok_r(p_copy + 5, ":", &rest));
+    char *cmd_path = path_concat((p_search) ? p_search : p_copy, cmd);
 
-    if (!path_copy)
-        return NULL;
-    search_path = strtok_r(path_copy + 5, ":", &checkpoint);
-    cmd_path = path_concat(search_path, cmd);
     while (cmd_path && access(cmd_path, F_OK)) {
-        search_path = strtok_r(NULL, ":", &checkpoint);
-        if (!search_path)
+        p_search = strtok_r(NULL, ":", &rest);
+        if (!p_search)
             break;
         free(cmd_path);
-        cmd_path = path_concat(search_path, cmd);
+        cmd_path = path_concat(p_search, cmd);
     }
     if (!access(cmd_path, F_OK))
         return cmd_path;
